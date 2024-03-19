dda1ebd5496387de5807927f0965e9be3b85d886
Sebastian Arcila <sarcila1998@gmail.com>
2024-03-19 16:24:28 -0400

    add loading spinner and content for when propertyanalyses array is null

--- src/app/pages/custom/property-list/property-list.component.ts
+++ src/app/pages/custom/property-list/property-list.component.ts
@@ -11,43 +11,44 @@ import { UtilityService } from 'src/app/services/utility.service';
 })
 export class PropertyListComponent implements OnInit {
   propertyAnalyses: PropertyAnalysis[];
+  isLoading: boolean;
 
-  propertyAnalysisExample: PropertyAnalysis = {
-    id: "1",
-    formattedAddress: "123 Main St, Anytown, AN 12345",
-    propertyType: "Multi-Family",
-    units: 2,
-    bedrooms: 3,
-    bathrooms: 2,
-    squareFootage: 1500,
-    purchasePrice: 300000,
-    isPurchasePriceWithinCriteria: true,
-    cashNeeded: 60000,
-    isCashNeededWithinCriteria: true,
-    cashFlow: 500,
-    isCashFlowWithinCriteria: false,
-    cashOnCashReturn: 8.78,
-    isCashOnCashReturnWithinCriteria: true,
-    capRate: 5,
-    isCapRateWithinCriteria: false,
-    internalRateOfReturn: 10,
-    isInternalRateOfReturnWithinCriteria: true,
-    rentalIncome: 2000,
-    expenses: 1500,
-    netOperatingIncome: 5000,
-    debtCoverageRatio: 1.2,
-    mortgage: 1000,
-    propertyTaxes: 300,
-    insurance: 200,
-    vacancy: 100,
-    repairAndMaintenance: 150,
-    capitalExpenditure: 100,
-    managementFees: 200,
-    otherExpenses: 50,
-    isSaved: false,
-    address: null,
-    rentComparables: null,
-  };
+  // propertyAnalysisExample: PropertyAnalysis = {
+  //   id: "1",
+  //   formattedAddress: "123 Main St, Anytown, AN 12345",
+  //   propertyType: "Multi-Family",
+  //   units: 2,
+  //   bedrooms: 3,
+  //   bathrooms: 2,
+  //   squareFootage: 1500,
+  //   purchasePrice: 300000,
+  //   isPurchasePriceWithinCriteria: true,
+  //   cashNeeded: 60000,
+  //   isCashNeededWithinCriteria: true,
+  //   cashFlow: 500,
+  //   isCashFlowWithinCriteria: false,
+  //   cashOnCashReturn: 8.78,
+  //   isCashOnCashReturnWithinCriteria: true,
+  //   capRate: 5,
+  //   isCapRateWithinCriteria: false,
+  //   internalRateOfReturn: 10,
+  //   isInternalRateOfReturnWithinCriteria: true,
+  //   rentalIncome: 2000,
+  //   expenses: 1500,
+  //   netOperatingIncome: 5000,
+  //   debtCoverageRatio: 1.2,
+  //   mortgage: 1000,
+  //   propertyTaxes: 300,
+  //   insurance: 200,
+  //   vacancy: 100,
+  //   repairAndMaintenance: 150,
+  //   capitalExpenditure: 100,
+  //   managementFees: 200,
+  //   otherExpenses: 50,
+  //   isSaved: false,
+  //   address: null,
+  //   rentComparables: null,
+  // };
 
   constructor(
     private router: Router,
@@ -59,14 +60,18 @@ export class PropertyListComponent implements OnInit {
   }
   
   ngOnInit(): void {
+    this.isLoading = true;
     this.propertyAnalysisService.getAllPropertiesAnalysis('ED8FCF47-66EC-4BED-9ADE-44602830AA65').subscribe({
       next: (data) => {
         console.log('getAllPropertiesAnalysis response: ', data);
         this.propertyAnalyses = data;
+        this.isLoading = false;
         this.cdr.detectChanges();
       },
       error: (error) => {
         console.error('getAllPropertiesAnalysis Error', error);
+        this.isLoading = false;
+        this.cdr.detectChanges();
         this.utilityService.showError();
       },
       complete: () => {
--- src/app/pages/custom/saved-property-list/saved-property-list.component.ts
+++ src/app/pages/custom/saved-property-list/saved-property-list.component.ts
@@ -11,43 +11,44 @@ import { UtilityService } from 'src/app/services/utility.service';
 })
 export class SavedPropertyListComponent implements OnInit {
   propertyAnalyses: PropertyAnalysis[];
+  isLoading: boolean;
 
-  propertyAnalysisExample: PropertyAnalysis = {
-    id: "1",
-    formattedAddress: "123 Main St, Anytown, AN 12345",
-    propertyType: "Multi-Family",
-    units: 2,
-    bedrooms: 3,
-    bathrooms: 2,
-    squareFootage: 1500,
-    purchasePrice: 300000,
-    isPurchasePriceWithinCriteria: true,
-    cashNeeded: 60000,
-    isCashNeededWithinCriteria: true,
-    cashFlow: 500,
-    isCashFlowWithinCriteria: false,
-    cashOnCashReturn: 8.78,
-    isCashOnCashReturnWithinCriteria: true,
-    capRate: 5,
-    isCapRateWithinCriteria: false,
-    internalRateOfReturn: 10,
-    isInternalRateOfReturnWithinCriteria: true,
-    rentalIncome: 2000,
-    expenses: 1500,
-    netOperatingIncome: 5000,
-    debtCoverageRatio: 1.2,
-    mortgage: 1000,
-    propertyTaxes: 300,
-    insurance: 200,
-    vacancy: 100,
-    repairAndMaintenance: 150,
-    capitalExpenditure: 100,
-    managementFees: 200,
-    otherExpenses: 50,
-    isSaved: false,
-    address: null,
-    rentComparables: null,
-  };
+  // propertyAnalysisExample: PropertyAnalysis = {
+  //   id: "1",
+  //   formattedAddress: "123 Main St, Anytown, AN 12345",
+  //   propertyType: "Multi-Family",
+  //   units: 2,
+  //   bedrooms: 3,
+  //   bathrooms: 2,
+  //   squareFootage: 1500,
+  //   purchasePrice: 300000,
+  //   isPurchasePriceWithinCriteria: true,
+  //   cashNeeded: 60000,
+  //   isCashNeededWithinCriteria: true,
+  //   cashFlow: 500,
+  //   isCashFlowWithinCriteria: false,
+  //   cashOnCashReturn: 8.78,
+  //   isCashOnCashReturnWithinCriteria: true,
+  //   capRate: 5,
+  //   isCapRateWithinCriteria: false,
+  //   internalRateOfReturn: 10,
+  //   isInternalRateOfReturnWithinCriteria: true,
+  //   rentalIncome: 2000,
+  //   expenses: 1500,
+  //   netOperatingIncome: 5000,
+  //   debtCoverageRatio: 1.2,
+  //   mortgage: 1000,
+  //   propertyTaxes: 300,
+  //   insurance: 200,
+  //   vacancy: 100,
+  //   repairAndMaintenance: 150,
+  //   capitalExpenditure: 100,
+  //   managementFees: 200,
+  //   otherExpenses: 50,
+  //   isSaved: false,
+  //   address: null,
+  //   rentComparables: null,
+  // };
 
   constructor(
     private router: Router,
@@ -59,15 +60,18 @@ export class SavedPropertyListComponent implements OnInit {
     }
   
   ngOnInit(): void {
-    //getAllSavedPropertiesAnalaysis()
+    this.isLoading = true;
     this.propertyAnalysisService.getAllSavedPropertiesAnalysis('ED8FCF47-66EC-4BED-9ADE-44602830AA65').subscribe({
       next: (data) => {
         console.log('getAllSavedPropertiesAnalysis response: ', data);
         this.propertyAnalyses = data;
+        this.isLoading = false;
         this.cdr.detectChanges();
       },
       error: (error) => {
         console.error('getAllSavedPropertiesAnalysis Error', error);
+        this.isLoading = false;
+        this.cdr.detectChanges();
         this.utilityService.showError();
       },
       complete: () => {
